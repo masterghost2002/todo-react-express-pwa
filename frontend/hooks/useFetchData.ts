dd488c32b3441ef88d94653a8c5e859a6f8f201a
Rakesh Dhariwal <rakeshdhariwal61@gmail.com>
2023-09-12 15:22:10 +0530

    offline mode

--- frontend/hooks/useFetchData.ts
+++ frontend/hooks/useFetchData.ts
@@ -46,7 +46,7 @@ export default function useFetchData(): resfetchReturnType {
     if(!isOnline) return;
     try {    
       const authReq = axios.create({ headers: { token: `Bearer ${localStorage.getItem('accessToken')}`}});      
-      const res = await authReq.get('https://calm-pink-chinchilla-tie.cyclic.app/api/todos/');
+      const res = await authReq.get('https://todo-api-toz9.onrender.com/api/todos/');
       const data = res.data;
       const { todos, ...user }: { todos: Todo[], user: User } = data;
       if (todos)
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,36 +4,39 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import { VitePWA } from "vite-plugin-pwa";
 const manifestForPlugIn = {
-  "theme_color": "#FFFFFF",
-  "background_color": "#FFFFFF",
-  "display": "standalone",
-  "scope": "/",
-  "start_url": "/",
-  "name": "Todo",
-  "description": "A basic todo app build over NextJS",
-  "short_name": "Todo",
-  "icons": [
-    {
-      "src": "/icon-192x192.png",
-      "sizes": "192x192",
-      "type": "image/png"
-    },
-    {
-      "src": "/icon-256x256.png",
-      "sizes": "256x256",
-      "type": "image/png"
-    },
-    {
-      "src": "/icon-384x384.png",
-      "sizes": "384x384",
-      "type": "image/png"
-    },
-    {
-      "src": "/icon-512x512.png",
-      "sizes": "512x512",
-      "type": "image/png"
-    }
-  ]
+  includeAssests: ['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  manifest: {
+    "theme_color": "#FFFFFF",
+    "background_color": "#FFFFFF",
+    "display": "standalone",
+    "scope": "/",
+    "start_url": "/",
+    "name": "Todo",
+    "description": "A basic todo app build over NextJS",
+    "short_name": "Todo",
+    "icons": [
+      {
+        "src": "/icon-192x192.png",
+        "sizes": "192x192",
+        "type": "image/png"
+      },
+      {
+        "src": "/icon-256x256.png",
+        "sizes": "256x256",
+        "type": "image/png"
+      },
+      {
+        "src": "/icon-384x384.png",
+        "sizes": "384x384",
+        "type": "image/png"
+      },
+      {
+        "src": "/icon-512x512.png",
+        "sizes": "512x512",
+        "type": "image/png"
+      }]
+  }
+
 }
 // https://vitejs.dev/config/
 export default defineConfig({

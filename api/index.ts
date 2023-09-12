099a1968262e98710071f05dfa05a4a012596b10
Rakesh Dhariwal <rakeshdhariwal61@gmail.com>
2023-09-12 13:52:46 +0530

    cros fixes

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,11 @@
 require('dotenv').config();
 import express, {Request, Response} from 'express';
+const app = express();
+let corsOptions = {
+    origin: [ 'https://todo-react-express-pwa.vercel.app/', 'http://localhost:3000', 'http://localhost:5173/' ]
+};
+import cors from 'cors';
+app.use(cors(corsOptions));
 import getTodo from './controller/get-todos/route';
 import deleteTodo from './controller/delete-todo/route';
 import postTodo from './controller/create-todo/route';
@@ -7,11 +13,8 @@ import putTask from './controller/update-todo/update-task/route';
 import putTodo from './controller/update-todo/route';
 import postInitalProfile from './controller/init-profile/route';
 const PORT = process.env.PORT || 5000;
-import cors from 'cors';
 import {google_auth, auth_token} from './auth';
-const app = express();
 app.use(express.json());
-app.use(cors());
 app.use('/api/user', google_auth);
 app.use('/api/todos', auth_token);
 app.get('/', (req:Request, res:Response)=>{
